5797435ca4af8b2f0017af5d7c1412dedf903127
czuniga <czuniga@espol.edu.ec>
2017-08-26 12:29:25 -0500

    Propietario-Admin(RD)

--- admin/modelo/propietario/form_EditPropietario.php
+++ admin/modelo/propietario/form_EditPropietario.php
@@ -1,16 +1,16 @@
 <?php
     session_start();
-    include_once("ClienteCollector.php");
-    include_once("Cliente.php");
-    $clienteCollectorObj = new ClienteCollector();
-    $id_cliente = $_GET['id_cliente'];
-    $cliente = $clienteCollectorObj->showCliente($id_cliente);
+    include_once("PropietarioCollector.php");
+    include_once("Propietario.php");
+    $propietarioCollectorObj = new PropietarioCollector();
+    $id_propietario = $_GET['id_propietario'];
+    $nombre = $propietarioCollectorObj->showPropietario($id_propietario);
 ?>
 
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Editar Cliente</title>
+	<title>Editar Propietario</title>
     <link href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
     <link href='https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css' rel='stylesheet' type='text/css'>
     <link href='../../modal.css' rel='stylesheet' type='text/css'>
@@ -27,26 +27,34 @@
       </div>
       <div class="panel-body">
         <div class="col-md-6 col-md-offset-3">
-          <form method="post" action="updateCliente.php?usuario=<?php $usuario?>">
+          <form method="post" action="updatePropietario.php?nombre=<?php $nombre?>">
             <div class="form-group">
                 <label for="username">ID</label>
-                <input type="text" class="form-control" name="id_cliente" readonly='readonly' required value=<?php echo $cliente->get_id_cliente(); ?>>
+                <input type="text" class="form-control" name="id_propietario" required value=<?php echo $id_propietario->get_id_propietario(); ?>>
             </div>  
             <div class="form-group">
-                <label for="username">Usuario</label>
-                <input type="text" class="form-control" name="id_usuario" readonly='readonly' required value=<?php echo $cliente->get_id_usuario(); ?>>
-            </div>
-            <div class="form-group">
                 <label for="username">Nombre</label>
-                <input type="text" class="form-control" name ="nombre" required  value=<?php echo $cliente->get_nombre(); ?>>
+                <input type="text" class="form-control" name="nombreModificado" required value=<?php echo $nombre->get_nombre(); ?>>
             </div>
             <div class="form-group">
                 <label for="username">Apellido</label>
                 <input type="text" class="form-control" name ="apellido" required  value=<?php echo $cliente->get_apellido(); ?>>
             </div>
+            <div class="form-group">
+                <label for="username">RUC</label>
+                <input type="text" class="form-control" name ="ruc" required  value=<?php echo $ruc->get_ruc(); ?>>
+            </div>
+            <div class="form-group">
+                <label for="username">NUMERO CUENTA</label>
+                <input type="text" class="form-control" name ="numerocuenta" required  value=<?php echo $numerocuenta->get_numerocuenta(); ?>>
+            </div>
+            <div class="form-group">
+                <label for="username">ESTADO</label>
+                <input type="text" class="form-control" name ="estado" required  value=<?php echo $estado->get_estado(); ?>>
+            </div>
             <div class="form-group clearfix">
               <button type="submit" class="btn btn-primary">Guardar</button>
-              <a href="readCliente.php" class="btn btn-info pull-right">Salir</a>       
+              <a href="readPropietario.php" class="btn btn-info pull-right">Salir</a>       
             </div>
         </form>
         </div>
