7c40ddf6074c527f47feef75c08bc94be6d1b723
lcoronelacosta <lufecoro@outlook.com>
2017-08-27 04:14:16 -0500

    Read and Create Reservas con modal, PHP y BD

--- admin/modelo/reservas/saveReserva.php
+++ admin/modelo/reservas/saveReserva.php
@@ -1,12 +1,12 @@
 <?php
     session_start();
-    include_once("VehiculoCollector.php");
-    include_once("Vehiculo.php");
+    include_once("ReservaCollector.php");
+    include_once("Reserva.php");
 	$descripcion = $_POST['descripcion'];
 	$placa = $_POST['placa'];
     $tipo = $_POST['tipo'];
     $id_cliente = $_POST['id_cliente'];
-    $vehiculoCollectorObj = new VehiculoCollector();
+    $reservaCollectorObj = new ReservaCollector();
 ?>
 
 <!DOCTYPE html>
@@ -17,18 +17,10 @@
     <body>
   
         <?php
-        //$roll = substr ("$rol", 0,1);
-        if($vehiculoCollectorObj->buscarVehiculo($placa)){
-            $mensaje = "ERROR EL VEHICULO YA SE ENCUENTRA REGISTRADO";
-            print "<script>alert('$mensaje')</script>";
-            echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=form_AddVehiculo.php'>";
-        }
-        else{
-        $vehiculoCollectorObj->createVehiculo($descripcion, $placa, $tipo, $id_cliente);
-        $mensaje = "EL VEHICULO SE CREO EXITOSAMENTE";
+        $reservaCollectorObj->createReserva($descripcion, $placa, $tipo, $id_cliente);
+        $mensaje = "LA RESERVA SE CREO EXITOSAMENTE";
         print "<script>alert('$mensaje')</script>";
-        echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=readVehiculo.php'>";
-        }
+        echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=readReserva.php'>";
         ?>
   
     </body>
