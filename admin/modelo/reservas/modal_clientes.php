2b29d2346bbadb95f317d4cc6802281b935838a8
lcoronelacosta <lufecoro@outlook.com>
2017-08-29 07:26:30 -0500

    Prueba CDN Bootstrap

--- admin/modelo/reservas/modal_clientes.php
+++ admin/modelo/reservas/modal_clientes.php
@@ -4,9 +4,6 @@
 ?>
 <html>
     <head>
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
         <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static">
@@ -63,4 +60,6 @@
     </div>
 </div>
     </body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </html>
