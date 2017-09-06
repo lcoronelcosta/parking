6301efecdb92f89654b96abdc671f11b6037c215
cdmitedaniel <cdmitedaniel@gmail.com>
2017-09-06 14:26:45 -0500

    Avance

--- admin/modelo/multa/form_EditMulta.php
+++ admin/modelo/multa/form_EditMulta.php
@@ -3,7 +3,8 @@
     include_once("MultaCollector.php");
     $multaCollectorObj = new MultaCollector();
     $id_multa = $_GET['id_multa'];
-    $descripcion = $_GET['descripcion'];
+    $descipcion = $_GET['descipcion'];
+    $valor = $_GET['valor'];
 ?>
 
 <html>
@@ -21,23 +22,23 @@
       <div class="panel-heading">
         <strong>
           <span class="glyphicon glyphicon-th"></span>
-          <span>Editar Vehiculo</span>
+          <span>Editar Multa</span>
        </strong>
       </div>
       <div class="panel-body">
         <div class="col-md-6 col-md-offset-3">
-          <form method="post" action="updateMulta.php?descripcion=<?php $descripcion?>">
+          <form method="post" action="updateMulta.php?descipcion=<?php $descipcion?>">
             <div class="form-group">
                 <label for="username">ID</label>
                 <input type="text" class="form-control" name="id_multa" required value=<?php echo "$id_multa"; ?>>
             </div>  
             <div class="form-group">
                 <label for="username">DESCRIPCION</label>
-                <input type="text" class="form-control" name="multaModificado" required value=<?php echo "$descipcion"; ?>>
+                <input type="text" class="form-control" name="descipcion" required value=<?php echo "$descipcion"; ?>>
             </div>
             <div class="form-group">
                 <label for="username">VALOR</label>
-                <input type="text" class="form-control" name="multaModificado" required value=<?php echo "$valor"; ?>>
+                <input type="text" class="form-control" name="valor" required value=<?php echo "$valor"; ?>>
             </div>
             <div class="form-group clearfix">
               <button type="submit" class="btn btn-primary">Guardar</button>
