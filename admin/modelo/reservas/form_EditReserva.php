a9a65506cd2e913a1e7fcf297e7e9c489be21d5c
czuniga <czuniga@espol.edu.ec>
2017-09-13 10:55:10 -0500

    Cambios

--- admin/modelo/reservas/form_EditReserva.php
+++ admin/modelo/reservas/form_EditReserva.php
@@ -26,7 +26,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Reserva</title>
-    <link href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
     <link href='https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css' rel='stylesheet' type='text/css'>
 </head>
 <body>
