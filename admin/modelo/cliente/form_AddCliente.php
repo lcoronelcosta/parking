cbab73761e3b3b6407f489ee2e7f583ffcfccec9
Luis Coronel <lufecoro@outlook.com>
2017-09-15 15:58:01 -0500

    Cambios

--- admin/modelo/cliente/form_AddCliente.php
+++ admin/modelo/cliente/form_AddCliente.php
@@ -6,11 +6,12 @@
 <head>
 	<meta charset="utf-8">
 	<title>Cliente</title>
-    <link href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
     <link href='https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css' rel='stylesheet' type='text/css'>
-    <link href='../../modal.css' rel='stylesheet' type='text/css'>
 </head>
 <body>
+
+    <?php require('header.php');?>
    <div class="row ">
     <div class="panel panel-default">
       <div class="panel-heading">
