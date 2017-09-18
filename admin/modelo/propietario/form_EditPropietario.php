fcf0b2743689eee4770462caff7cea59c65a6b23
czuniga <czuniga@espol.edu.ec>
2017-09-18 07:50:52 -0500

    etiquetas

--- admin/modelo/propietario/form_EditPropietario.php
+++ admin/modelo/propietario/form_EditPropietario.php
@@ -20,7 +20,7 @@
       <div class="panel-heading">
         <strong>
           <span class="glyphicon glyphicon-th"></span>
-          <span>Editar Propietario</span>
+          <span>EDITAR PROPIETARIO</span>
        </strong>
       </div>
       <div class="panel-body">
--- admin/modelo/vehiculo/form_EditVehiculo.php
+++ admin/modelo/vehiculo/form_EditVehiculo.php
@@ -29,7 +29,7 @@
       <div class="panel-heading">
         <strong>
           <span class="glyphicon glyphicon-th"></span>
-          <span>Editar Vehiculo</span>
+          <span>EDITAR VEHICULO</span>
        </strong>
       </div>
       <div class="panel-body">
