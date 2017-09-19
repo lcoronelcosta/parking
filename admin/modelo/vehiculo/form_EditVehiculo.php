028b40711d57a23668c597e89650fa7990ed9910
lcoronelacosta <lufecoro@outlook.com>
2017-09-19 07:38:46 -0500

    Carga tipo de vehiculo

--- admin/modelo/vehiculo/form_EditVehiculo.php
+++ admin/modelo/vehiculo/form_EditVehiculo.php
@@ -65,12 +65,12 @@
               <label for="level">Tipo Vehiculo</label>
                 <select class="form-control" name="tipo">
                   <?php
-                    if ('$tipo' == 'M') {
+                    if ($tipo == 'M') {
                         echo "<option>Moto</option>";
                         echo "<option>Auto</option>";
                         echo "<option>Camioneta</option>";
                     }
-                    elseif ('$tipo' == 'C') {
+                    elseif ($tipo == 'C') {
                         echo "<option>Camioneta</option>";
                         echo "<option>Moto</option>";
                         echo "<option>Auto</option>";
